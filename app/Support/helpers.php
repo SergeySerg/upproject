5484a734a90affb94bc73145101d983ebb94e440
Sergey <vor.ser87@gmail.com>
2017-02-11 18:41:33 +0200

    create file helpers

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,15 +1,33 @@
 <?php
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Backend;
+//use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Request;
+//use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\URL;
+use App\Models\Article;
+use App\Models\Category;
+use App\Models\Lang;
+use App\Models\Translate;
+use App\Models\Text;
+use App\Models\Setting;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\MessageBag;
 
+/*Function for get Setting content*/
 if (! function_exists('getSetting')) {
     /**
      * Format text.
      *
-     * @param  string  $text
+     * @param  string  $value
      * @return string
      */
-    function getSetting($text)
+    function getSetting($value)
     {
+        $setting = Setting::where('name',$value)->first();
+        $setting_content = $setting->description;
         // Format text
-        return $text;
+        return $setting_content;
     }
 }
